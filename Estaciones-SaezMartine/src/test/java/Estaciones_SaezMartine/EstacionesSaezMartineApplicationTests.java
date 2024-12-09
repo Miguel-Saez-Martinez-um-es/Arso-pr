ccd68e5ff4a94e4c5b70cbad1cfefb39e6689c79
Saez <miguel.saezm@um.es>
2024-12-09 14:12:19 +0100

    Ejercicio 2 apartado 1 en proceso 2.0

---
+++ Estaciones-SaezMartine/src/test/java/Estaciones_SaezMartine/EstacionesSaezMartineApplicationTests.java
@@ -0,0 +1,13 @@
+package Estaciones_SaezMartine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionesSaezMartineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
